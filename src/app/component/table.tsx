1d8c064a30e1585e1d4d3fd1ec60267735a53e8e
ChrisWen960216 <chriswen96@163.com>
2017-12-27 10:47:20 +0800

    Completed TableComponent UI

--- src/app/component/table.tsx
+++ src/app/component/table.tsx
@@ -1,14 +1,48 @@
 import * as React from 'react';
 import { connect } from 'react-redux';
+import { Table, Row, Col } from 'antd';
 
 import { loadUserList } from '../../redux/list/action';
 
 const { Component }: typeof React = React;
 
-class TableComponent extends Component {
+const Columns: Array<object> = [{
+  title: '姓名',
+  dataIndex: 'name',
+  key: 'name',
+}, {
+  title: '工号',
+  dataIndex: 'uid',
+  key: 'uid',
+}, {
+  title: '电话',
+  dataIndex: 'phone',
+  key: 'phone',
+  }];
+
+class TableComponent extends Component<any, any> {
+  constructor(props: any) {
+    super(props);
+    this.state = {
+      dataSource: [
+        {
+          name: '文韬',
+          uid: '012404',
+          phone: '17521100216'
+        }
+      ]
+    };
+  }
   render(): JSX.Element {
+    
     return (
-      <div>123</div>
+      <Row>
+        <Col span={4} />
+        <Col span={16}>
+          <Table columns={Columns} dataSource={this.state.dataSource} bordered={true}/>
+        </Col>  
+        <Col span={4}/>
+      </Row>
     );
   }
 }
--- src/app/container/main.tsx
+++ src/app/container/main.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import HeaderComponent from '../component/header';
 import InputComponent from '../component/input';
+import TableComponent from '../component/table';
 
 export default class MainPage extends React.Component {
   render(): JSX.Element {
@@ -8,6 +9,7 @@ export default class MainPage extends React.Component {
       <div id="root">
         <HeaderComponent />
         <InputComponent />
+        <TableComponent />
       </div>
     );
   }
